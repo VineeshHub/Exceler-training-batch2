1c44e74bf53da8d4abd4dc04f8dc0e9629ea17e8
VineeshHub <2311cs010641@mallareddyuniversity.ac.in>
2025-01-03 10:16:49 +0530

    Add files via upload

---
+++ demo06.java
@@ -0,0 +1,10 @@
+public class demo06 {
+    public static void main(String[] args){
+        String username = "Vineesh";
+        String pwd ="Vineesh_1234";
+        while((username=="Vineesh")&& (pwd=="Vineesh_1234")){
+            System.out.println("hello Vineesh welcome to mallareddy university");
+            break;
+        }
+    }
+}
