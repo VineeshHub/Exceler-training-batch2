e4de9499efdef6e3cf45c33405a2cabb852ed821
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-26 21:08:27 +0530

    Sum of digits
    Sum of digits

--- demo017.java
+++ demo017.java
@@ -1,30 +1,17 @@
 import java.util.Scanner;
 
-public class demo017 {
+public class Demo017 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String loginid = "";
-        String pwd = "";
-        String reply;
-
-        do {
-            System.out.println("Welcome to Malla Reddy University");
-            System.out.println("Have you visited our site earlier? (yes/no)");
-            reply = scan.nextLine();
-
-            if (reply == "no") {
-                System.out.println("Login here");
-                System.out.println("Enter your ID:");
-                loginid = scan.nextLine();
-                System.out.println("Enter your password:");
-                pwd = scan.nextLine();
-            } else {
-                System.out.println("You are our valued customer.");
-            }
-
-        } while (!loginid.equals("Srinath") || !pwd.equals("123456")); // Loop until valid credentials
-
-        System.out.println("Login successful!");
-        scan.close();
-    }
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a two-digit number: ");
+        int number = scanner.nextInt();
+    
+        int tens = number / 10;  
+        int ones = number % 10;  
+        
+        int sum = tens + ones;
+        System.out.println(tens + " + " + ones + " = " + sum);
+        
+        }
 }
