f049abec8dea1db26661ac07f34680d4024b800f
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-26 20:57:19 +0530

    Unary Operators
    Unary Operators

--- demo014.java
+++ demo014.java
@@ -1,24 +1,19 @@
 public class demo014 {
-    
-        public static void main(String[] args) {
-            int a = 10;
-    
-            System.out.println("Initial value of a: " + a);
-    
-            // Post-increment
-            System.out.println("Post-increment (a++): " + a++); // Prints 10, then increments a to 11
-            System.out.println("Value of a after post-increment: " + a);
-    
-            // Pre-increment
-            System.out.println("Pre-increment (++a): " + ++a); // Increments a to 12, then prints 12
-            System.out.println("Value of a after pre-increment: " + a);
-    
-            // Post-decrement
-            System.out.println("Post-decrement (a--): " + a--); // Prints 12, then decrements a to 11
-            System.out.println("Value of a after post-decrement: " + a);
-    
-            // Pre-decrement
-            System.out.println("Pre-decrement (--a): " + --a); // Decrements a to 10, then prints 10
-            System.out.println("Value of a after pre-decrement: " + a);
-        }
+    public static void main(String[] args) {
+        int a = 10;
+        int b = -a;  
+        int c = +a;  
+        int d = ++a; 
+        int e = a++; 
+        int f = --a; 
+        int g = a--; 
+
+        System.out.println("Original value of a: " + a);
+        System.out.println("Unary minus (-a): " + b);
+        System.out.println("Unary plus (+a): " + c);
+        System.out.println("Pre-increment (++a): " + d);
+        System.out.println("Post-increment (a++): " + e);
+        System.out.println("Pre-decrement (--a): " + f);
+        System.out.println("Post-decrement (a--): " + g);
     }
+}
