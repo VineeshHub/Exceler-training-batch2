2c620ba9ca096d34d64c059d4d245815914d1e11
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 09:56:09 +0530

    Update and rename Demo040.java to Demo40.java

--- Demo40.java
+++ Demo40.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo040 {
+public class Demo40 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
