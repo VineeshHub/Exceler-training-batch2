15cea67944666d867603290aebef8a9828ee93dd
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 10:20:55 +0530

    Update and rename LTCode005.java to LTCode05.java

--- LTCode05.java
+++ LTCode05.java
@@ -17,7 +17,7 @@ class Solution {
     }
 }
 
-class LTCode005{
+class LTCode05{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
