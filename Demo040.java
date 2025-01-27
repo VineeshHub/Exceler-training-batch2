0c26d826c9a0cea54b68df94ea69cd2a139089ba
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 09:56:46 +0530

    Lowercase Or Uppercase Or Digit Or Special
    Lowercase Or Uppercase Or Digit Or Special

--- Demo040.java
+++ Demo040.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo40 {
+public class Demo040 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
