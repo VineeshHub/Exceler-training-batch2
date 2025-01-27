73936334bcc65082a29658c1a03559e4ea3445c8
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 10:08:41 +0530

    Update and rename Demo049.java to Demo49.java

--- Demo49.java
+++ Demo49.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Demo049{
+class Demo49{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);   
         System.out.println("Enter the values of x: ");
