5c2c464619bec3f82b4bc6fa3eb4ac82e6b40bb0
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 10:21:24 +0530

    Sum of two numbers using leet code
    Sum of two numbers using leet code

--- LTCode005.java
+++ LTCode005.java
@@ -17,7 +17,7 @@ class Solution {
     }
 }
 
-class LTCode05{
+class LTCode005{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
