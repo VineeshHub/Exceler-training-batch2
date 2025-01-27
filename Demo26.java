b12c096455c057503bbccb99dd1a1a7c7c71ef11
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 09:40:29 +0530

    Update and rename Demo026.java to Demo26.java

--- Demo26.java
+++ Demo26.java
@@ -19,7 +19,7 @@ class Count{
     }
 }
 
-class Demo026{
+class Demo26{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string: ");
