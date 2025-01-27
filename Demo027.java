6048ec299e10df98e307753bfc721ef6032e0212
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 09:42:12 +0530

    Prime or Not
    Prime or Not

--- Demo027.java
+++ Demo027.java
@@ -22,7 +22,7 @@ class Prime{
     
 }
 
-class Demo27{
+class Demo027{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
