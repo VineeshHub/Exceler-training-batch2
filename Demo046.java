bd383238e38631a6255844baf90914b407b07b3c
Vineesh Reddy <2311cs010641@mallareddyuniversity.ac.in>
2025-01-27 10:05:00 +0530

    Check Year is Leap or not
    Check Year is Leap or not

--- Demo046.java
+++ Demo046.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Demo46{
+class Demo046{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a year: ");
